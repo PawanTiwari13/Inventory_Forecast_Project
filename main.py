f2c1c87d039c7d8afcddbfa5c83cb53ace8d9e70
91982 <pt349031@gmail.com>
2024-05-31 13:05:06 +0530

    Added Images in the Project

--- main.py
+++ main.py
@@ -4,13 +4,98 @@
 # Press Double Shift to search everywhere for classes, files, tool windows, actions, and settings.
 
 
-def print_hi(name):
-    # Use a breakpoint in the code line below to debug your script.
-    print(f'Hi, {name}')  # Press Ctrl+F8 to toggle the breakpoint.
+import os
+import time
+
+board = [' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ', ' ']
+player = 1
+
+# Win Flags
+Win = 1
+Draw = -1
+Running = 0
+Stop = 1
+
+Game = Running
+Mark = 'X'
 
 
-# Press the green button in the gutter to run the script.
-if __name__ == '__main__':
-    print_hi('PyCharm')
+def DrawBoard():
+    print(" %c | %c | %c " % (board[1], board[2], board[3]))
+    print("___|___|___")
+    print(" %c | %c | %c " % (board[4], board[5], board[6]))
+    print("___|___|___")
+    print(" %c | %c | %c " % (board[7], board[8], board[9]))
+    print(" | | ")
 
+
+def CheckPosition(x):
+    if board[x] == ' ':
+        return True
+    else:
+        return False
+
+
+def CheckWin():
+    global Game
+
+    if board[1] == board[2] and board[2] == board[3] and board[1] != ' ':
+        Game = Win
+    elif board[4] == board[5] and board[5] == board[6] and board[4] != ' ':
+        Game = Win
+    elif board[7] == board[8] and board[8] == board[9] and board[7] != ' ':
+        Game = Win
+    elif board[1] == board[4] and board[4] == board[7] and board[1] != ' ':
+        Game = Win
+    elif board[2] == board[5] and board[5] == board[8] and board[2] != ' ':
+        Game = Win
+    elif board[3] == board[6] and board[6] == board[9] and board[3] != ' ':
+        Game = Win
+    elif board[1] == board[5] and board[5] == board[9] and board[5] != ' ':
+        Game = Win
+    elif board[3] == board[5] and board[5] == board[7] and board[5] != ' ':
+        Game = Win
+    elif board[1] != ' ' and board[2] != ' ' and board[3] != ' ' and \
+            board[4] != ' ' and board[5] != ' ' and board[6] != ' ' and \
+            board[7] != ' ' and board[8] != ' ' and board[9] != ' ':
+        Game = Draw
+    else:
+        Game = Running
+
+
+print("Tic-Tac-Toe Game Designed By Ganesh Bonde")
+print("Player 1 [X] --- Player 2 [O]\n")
+print()
+print()
+print("Please Wait...")
+time.sleep(3)
+
+while Game == Running:
+    os.system('cls')
+    DrawBoard()
+
+    if player % 2 != 0:
+        print("Player 1's chance")
+        Mark = 'X'
+    else:
+        print("Player 2's chance")
+        Mark = 'O'
+
+    choice = int(input("Enter the position between [1-9] where you want to mark: "))
+    if CheckPosition(choice):
+        board[choice] = Mark
+        player += 1
+        CheckWin()
+
+    os.system('cls')
+    DrawBoard()
+
+    if Game == Draw:
+        print("Game Draw")
+    elif Game == Win:
+        player -= 1
+        if player % 2 != 0:
+            print("Player 1 Won")
+        else:
+            print("Player 2 Won")
 # See PyCharm help at https://www.jetbrains.com/help/pycharm/
